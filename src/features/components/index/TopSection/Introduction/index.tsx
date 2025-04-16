9ca9c68b95f2d5c043950f6b92e273088b8a46b2
itsu-dev <itsu020402@gmail.com>
2025-04-17 02:02:37 +0900

    feat: 所属を更新

--- src/features/components/index/TopSection/Introduction/index.tsx
+++ src/features/components/index/TopSection/Introduction/index.tsx
@@ -14,14 +14,31 @@ export default function Introduction() {
   );
 
   return (
-    <div ref={ref} className={styles.wrapper}>
-      <img className={styles.icon} src="/icon.svg" alt="自分のアイコン" />
-      <div className={styles.divider}></div>
-      <div className={styles.description}>
-        <h3>ちゅるり</h3>
-        <p className={styles.affiliation}>筑波大学情報メディア創成学類 4 年</p>
-        <p>たべること・寝ることがすき</p>
+    <div className={styles.wrapper}>
+      <div ref={ref} className={styles.flexWrapper}>
+        <img className={styles.icon} src="/icon.svg" alt="自分のアイコン" />
+        <div className={styles.divider}></div>
+        <div className={styles.description}>
+          <h3>ちゅるり</h3>
+          <p className={styles.affiliation}>筑波大学 情報理工学位プログラム M1</p>
+          <p>たべること・寝ることがすき</p>
+        </div>
       </div>
+      {/*<div className={styles.others}>*/}
+      {/*  <div className={styles.row}>*/}
+      {/*    <p className={styles.rowTitle}>TA</p>*/}
+      {/*    <p className={styles.rowContent}>*/}
+      {/*      <span className={`${styles.label} ${styles.labelCOJT}`}>COJT SW</span>*/}
+      {/*      <span className={`${styles.label} ${styles.labelPRO}`}>プログラミング</span>*/}
+      {/*    </p>*/}
+      {/*  </div>*/}
+      {/*  <div className={styles.row}>*/}
+      {/*    <p className={styles.rowTitle}>Working At</p>*/}
+      {/*    <p className={styles.rowContent}>*/}
+      {/*      <span>株式会社スプーキーズ</span>*/}
+      {/*    </p>*/}
+      {/*  </div>*/}
+      {/*</div>*/}
     </div>
   );
 }
